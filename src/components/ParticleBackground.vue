5b9a987578785354fde217c8088eccde6a2f27c0
shenjian-gaocheng <2351039@tongji.edu.cn>
2025-06-15 01:15:42 +0800

    背景粒子数动态变化

--- src/components/ParticleBackground.vue
+++ src/components/ParticleBackground.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-  <vue-particles id="tsparticles" :options="particlesOptions" />
+  <vue-particles id="tsparticles" :options="particlesOptions" @particlesLoaded="onParticlesLoaded" />
 </template>
 
 <script>
@@ -30,7 +30,7 @@ export default {
               duration: 0.4
             },
             push: {
-              quantity: 4
+              quantity: 4 // 每次点击增加的粒子数
             }
           }
         },
@@ -46,15 +46,13 @@ export default {
           move: {
             enable: true,
             speed: 1,
-            // random: true,
-            // outModes: 'bounce'
             direction: 'none',
             random: false,
             straight: false,
             outModes: { default: 'out' }
           },
           number: {
-            value: 100, // 粒子数量
+            value: 100,
             density: {
               enable: true,
               area: 800
@@ -65,15 +63,72 @@ export default {
             random: false
           },
           shape: {
-            type: 'circle' // 粒子形状为圆形
+            type: 'circle'
           },
           size: {
-            value: { min: 1, max: 3 }, // 粒子大小范围
+            value: { min: 1, max: 3 },
             random: true
           }
         },
         detectRetina: true
+      },
+      particlesInstance: null,
+      animationInterval: null,
+      baseMaxParticles: 100, // 基础最大粒子数
+      currentMaxParticles: 100, // 当前最大粒子数(会随点击增加)
+      minParticles: 30 // 最小粒子数
+    }
+  },
+  methods: {
+    onParticlesLoaded(instance) {
+      this.particlesInstance = instance;
+      this.startParticleAnimation();
+      
+      // 监听粒子数量变化(用于检测点击增加)
+      instance.particles.onCountChange((count) => {
+        if (count > this.currentMaxParticles) {
+          this.currentMaxParticles = count;
+        }
+      });
+    },
+    startParticleAnimation() {
+      if (this.animationInterval) {
+        clearInterval(this.animationInterval);
       }
+      
+      let direction = -1;
+      let currentParticles = this.currentMaxParticles;
+      
+      this.animationInterval = setInterval(() => {
+        // 更新粒子数量
+        currentParticles += direction * 2;
+        
+        // 确保不低于最小值，不高于当前最大值
+        if (currentParticles <= this.minParticles) {
+          currentParticles = this.minParticles;
+          direction = 1;
+        } else if (currentParticles >= this.currentMaxParticles) {
+          currentParticles = this.currentMaxParticles;
+          direction = -1;
+          
+          // 如果当前最大值比基础值大，则缓慢恢复到基础值
+          if (this.currentMaxParticles > this.baseMaxParticles) {
+            this.currentMaxParticles = Math.max(
+              this.baseMaxParticles,
+              this.currentMaxParticles - 1
+            );
+          }
+        }
+        
+        if (this.particlesInstance) {
+          this.particlesInstance.particles.setQuantity(currentParticles);
+        }
+      }, 100);
+    }
+  },
+  beforeDestroy() {
+    if (this.animationInterval) {
+      clearInterval(this.animationInterval);
     }
   }
 }
